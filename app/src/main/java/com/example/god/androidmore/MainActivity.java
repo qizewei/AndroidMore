c821b0838743a3e38e10b8e8d94b18e651b10406
Qzw's Mac <qizewei@vip.qq.com>
2018-12-13 16:59:24 +0800

    归并排序_Finish

--- app/src/main/java/com/example/god/androidmore/MainActivity.java
+++ app/src/main/java/com/example/god/androidmore/MainActivity.java
@@ -13,6 +13,7 @@ import static com.example.god.androidmore.algorithm.other.Recursion.Hanoi;
 import static com.example.god.androidmore.algorithm.other.Recursion.MaxDivisor;
 import static com.example.god.androidmore.algorithm.search.BinarySearch.binarySearch;
 import static com.example.god.androidmore.algorithm.sort.BubbleSort.BubbleSort;
+import static com.example.god.androidmore.algorithm.sort.MergeSort.mergeSort;
 import static com.example.god.androidmore.algorithm.sort.SelectionSort.selsctionSort;
 import static com.example.god.androidmore.algorithm.sort.ShellSort.shellSort;
 
@@ -58,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
 //    Log.d(TAG, "onCreate: 最大公约数为" + MaxDivisor(9, 4));
 
 //    泊松分酒
-    putDrive(12, 0, 0, 6, 12, 8, 5);
+//    putDrive(12, 0, 0, 6, 12, 8, 5);
+
+    mergeSort(src,0,src.length-1);
+    logOut("归并", src);
+
   }
 
   private void logOut(String method, int[] src) {
--- app/src/main/java/com/example/god/androidmore/algorithm/sort/MergeSort.java
+++ app/src/main/java/com/example/god/androidmore/algorithm/sort/MergeSort.java
@@ -8,10 +8,29 @@
 
 package com.example.god.androidmore.algorithm.sort;
 
+import static com.example.god.androidmore.algorithm.sort.InsertionSort.insertionSort;
+
 /**
  * 归并排序：二路归并排序
  * 将数组分每两个元素分成一组，然后两两排序并合并
  */
 public class MergeSort {
 
+  public static void mergeSort(int[] src, int start, int end) {
+    if (end-start == 1) {
+      if(src[start]>src[end])
+        replaceNum(src,start,end);
+      return;
+    }
+    int m =(start+end)>>1;
+    mergeSort(src, start, m);
+    mergeSort(src, m , end);
+    insertionSort(src);
+  }
+
+  public static void replaceNum(int[] src, int star, int end) {
+    int x = src[star];
+    src[star] = src[end];
+    src[end] = x;
+  }
 }
